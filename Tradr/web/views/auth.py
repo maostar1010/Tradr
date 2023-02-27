0235f738348ac04b8b8501e32cc07c2ed28a95bb
Mary <maostar1010@gmail.com>
2023-02-27 14:32:35 -0700

    typo correction in auth.py

--- Tradr/web/views/auth.py
+++ Tradr/web/views/auth.py
@@ -9,4 +9,7 @@ def login(response):
     return render(response, "web\login.html", {})
 
 def register(response):
+    if response.method =="POST":
+        email = response.POST.get('email')
+
     return render(response, "web\\register.html", {})
