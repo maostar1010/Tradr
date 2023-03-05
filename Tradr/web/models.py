159f447834c36d6d5cc320723e28f3c43af5170e
albinrad <radajalbin@gmail.com>
2023-03-05 14:06:37 -0700

    Implemented Listing and Image tables, added sample functionality to create new listing

--- Tradr/web/models.py
+++ Tradr/web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+    
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first())
+    title = models.CharField(max_length=200,null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7,decimal_places=2)
+    tag = models.CharField(max_length=200,null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+    
+class Image(models.Model):
+    listing = models.ForeignKey(Listing,on_delete=models.CASCADE, null=True)
+    image = models.ImageField(null=False,blank=False)
+
+    def __str__(self):
+        return f"Image for {self.listing.title}: {self.image.name}"
+
+
--- Tradr/web/urls.py
+++ Tradr/web/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import nav, view_auth
+from .views.create_listing import CreateListing
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns=[
@@ -7,6 +8,7 @@ urlpatterns=[
     path("login/", view_auth.login, name="login"),
     path("logout/", view_auth.logout, name="logout"),
     path("register/", view_auth.register, name="register"),
+    path('create_listing/', CreateListing.as_view(), name='create_listing'),
     # path("home/", nav.home, name="home"),
 ]
 
