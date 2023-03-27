bd93bc7a59634cc1f5e50074bc0ecfd3e719d429
Mary <maostar1010@gmail.com>
2023-03-26 18:35:09 -0600

    images under listing in admin pages

--- Tradr/web/admin.py
+++ Tradr/web/admin.py
@@ -3,11 +3,25 @@ from .models import *
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Listing)
-admin.site.register(Image)
+# admin.site.register(Listing)
+# admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(ListingCategory)
 admin.site.register(Review)
 admin.site.register(Complaint)
 admin.site.register(TransactionReport)
 admin.site.register(Condition)
+
+class ImageAdmin(admin.StackedInline):
+    model = Image
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    inlines = [ImageAdmin]
+
+    class Meta:
+        model = Listing
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
